04f763e93613a9bab85068fdc088f188a9d2a15f
Zalkar Tilenbaev <zalkarbek.tilenbaev.kk@gmail.com>
2023-03-11 16:06:32 +0600

    animation cubes w

--- lib/animations_cubes_widget.dart
+++ lib/animations_cubes_widget.dart
@@ -48,96 +48,97 @@ class _AnimationsCubesWidgetState extends State<AnimationsCubesWidget>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.deepPurple[300],
-        body: Center(
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              // biggest  object
-              Transform.rotate(
-                angle: _radiusAnimation.value,
-                child: Container(
-                  width: 225,
-                  height: 225,
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple[400],
-                    borderRadius: BorderRadius.circular(_radiusAnimation.value),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.8),
-                        offset: const Offset(-6.0, -6.0),
-                      ),
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        offset: const Offset(-6.0, -6.0),
-                      ),
-                    ],
-                  ),
+      backgroundColor: Colors.deepPurple[300],
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            // biggest  object
+            Transform.rotate(
+              angle: _radiusAnimation.value,
+              child: Container(
+                width: 225,
+                height: 225,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[400],
+                  borderRadius: BorderRadius.circular(_radiusAnimation.value),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.8),
+                      offset: const Offset(-6.0, -6.0),
+                    ),
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      offset: const Offset(-6.0, -6.0),
+                    ),
+                  ],
                 ),
               ),
+            ),
 
-              // 2nd biggest
-              Transform.rotate(
-                angle: _radiusAnimation.value + 0.2,
-                child: Container(
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple[500],
-                    borderRadius: BorderRadius.circular(_radiusAnimation.value),
-                  ),
+            // 2nd biggest
+            Transform.rotate(
+              angle: _radiusAnimation.value + 0.2,
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[500],
+                  borderRadius: BorderRadius.circular(_radiusAnimation.value),
                 ),
               ),
-              //3rd biggest
-              Transform.rotate(
-                angle: _rotationAnimation.value + 0.4,
-                child: Container(
-                  width: 180,
-                  height: 180,
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple[300],
-                    borderRadius: BorderRadius.circular(_radiusAnimation.value),
-                  ),
+            ),
+            //3rd biggest
+            Transform.rotate(
+              angle: _rotationAnimation.value + 0.4,
+              child: Container(
+                width: 180,
+                height: 180,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[300],
+                  borderRadius: BorderRadius.circular(_radiusAnimation.value),
                 ),
               ),
-              //4rd biggest
-              Transform.rotate(
-                angle: _rotationAnimation.value + 0.6,
-                child: Container(
-                  width: 160,
-                  height: 160,
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple[200],
-                    borderRadius: BorderRadius.circular(_radiusAnimation.value),
-                  ),
+            ),
+            //4rd biggest
+            Transform.rotate(
+              angle: _rotationAnimation.value + 0.6,
+              child: Container(
+                width: 160,
+                height: 160,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[200],
+                  borderRadius: BorderRadius.circular(_radiusAnimation.value),
                 ),
               ),
-              //5rd biggest
-              Transform.rotate(
-                angle: _rotationAnimation.value + 0.8,
-                child: Container(
-                  width: 140,
-                  height: 140,
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple[150],
-                    borderRadius: BorderRadius.circular(_radiusAnimation.value),
-                  ),
+            ),
+            //5rd biggest
+            Transform.rotate(
+              angle: _rotationAnimation.value + 0.8,
+              child: Container(
+                width: 140,
+                height: 140,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[150],
+                  borderRadius: BorderRadius.circular(_radiusAnimation.value),
                 ),
               ),
-              //6rd biggest
-              Transform.rotate(
-                angle: _rotationAnimation.value + 1,
-                child: Container(
-                  width: 120,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple[100],
-                    borderRadius: BorderRadius.circular(_radiusAnimation.value),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ));
+            ),
+            //6rd biggest
+            // Transform.rotate(
+            //   angle: _rotationAnimation.value + 1,
+            //   child: Container(
+            //     width: 120,
+            //     height: 120,
+            //     decoration: BoxDecoration(
+            //       color: Colors.deepPurple[100],
+            //       borderRadius: BorderRadius.circular(_radiusAnimation.value),
+            //     ),
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+    );
   }
 }
